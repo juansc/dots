702022c6c7464b9d9609c94f8b6fe788aa1a470d
Juan <juanscarrillo23@gmail.com>
2018-02-08 13:00:16 -0800

    removed statement that should be changed on a machine basis and updated colors

--- config.fish
+++ config.fish
@@ -1,9 +1,6 @@
 # Add go to path
 set PATH /usr/local/go/bin $HOME/git/go $PATH
 
-# Add rg to path
-set PATH $HOME/.cargo/bin $PATH
-
 set -x VIRTUAL_ENV_DISABLE_PROMPT 1
 
-set LSCOLORS gxBxhxDxfxhxhxhxhxcxcx
+set -gx LSCOLORS gxBxhxDxfxhxhxhxhxcxcx
