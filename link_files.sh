95dbefea2485a1d10d7478ca587fa98e353ee997
Juan <juanscarrillo23@gmail.com>
2023-10-20 18:34:35 -0700

    fixed link script

--- link_files.sh
+++ link_files.sh
@@ -4,7 +4,7 @@ DOTS_DIR=${HOME}/git/dots
 ln -sf ${DOTS_DIR}/.alacritty.yml ${HOME}/.alacritty.yml
 ln -sf ${DOTS_DIR}/kitty/kitty.conf ${HOME}/.config/kitty/kitty.conf
 # git
-ln -sf ${DOTS_DIR}/.gitconfig ${HOME}/.gitconfig
+ln -sf ${DOTS_DIR}/git/.gitconfig ${HOME}/.gitconfig
 ln -sf ${DOTS_DIR}/.gitignore_global ${HOME}/.gitignore_global
 # vim stuff
 ln -sf ${DOTS_DIR}/vim/.vimrc ${HOME}/.vimrc
@@ -15,6 +15,6 @@ ln -sf ${DOTS_DIR}/vim/gruvbox8_hard.vim ${HOME}/.vim/colors/gruvbox8_hard.vim
 # tmux
 ln -sf ${DOTS_DIR}/.tmux.conf ${HOME}/.tmux.conf
 # lazygit
-ln -sf ${DOTS_DIR}/.tmux.conf ${HOME}/.config/lazygit.yml
+ln -sf ${DOTS_DIR}/lazygit/lazygit.yml ${HOME}/.config/lazygit.yml
 # custom functions
 ln -sf ${DOTS_DIR}/.zfunc ${HOME}/.zfunc
