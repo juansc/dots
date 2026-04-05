cac4d8d6ead8918e5b9a762b0c92518db7a935f2
Juan <juanscarrillo23@gmail.com>
2026-04-04 18:49:26 -0700

    some updates to wezterm and iterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,6 +4,9 @@ local act = wezterm.action
 local config = wezterm.config_builder()
 
 config.scrollback_lines = 10000
+config.default_prog = { '/opt/homebrew/bin/fish'}
+config.enable_scroll_bar = true
+
 
 -- Add splits from iterm
 config.keys = {
@@ -54,5 +57,63 @@ config.keys = {
     mods = 'SUPER|ALT',
     action = act.ActivatePaneDirection 'Down',
   },
+  {
+    key = '[',
+    mods = 'SUPER',
+    action = act.ActivatePaneDirection 'Prev',
+  },
+  {
+    key = ']',
+    mods = 'SUPER',
+    action = act.ActivatePaneDirection 'Next',
+  },
+  -- Match word skipping behavior in Terminal
+    -- Rebind OPT-Left, OPT-Right as ALT-b, ALT-f respectively to match Terminal.app behavior
+  {
+    key = 'LeftArrow',
+    mods = 'OPT',
+    action = act.SendKey {
+      key = 'b',
+      mods = 'ALT',
+    },
+  },
+  {
+    key = 'RightArrow',
+    mods = 'OPT',
+    action = act.SendKey { key = 'f', mods = 'ALT' },
+  },
 }
+
+config.mouse_bindings = {
+    -- Disable the default click behavior
+    {
+      event = { Up = { streak = 1, button = "Left"} },
+      mods = "NONE",
+      action = wezterm.action.DisableDefaultAssignment, -- You may want a different option here. See /u/Brian's comment below
+    },
+    -- Ctrl-click will open the link under the mouse cursor
+    {
+        event = { Up = { streak = 1, button = "Left" } },
+        mods = "CTRL",
+        action = wezterm.action.OpenLinkAtMouseCursor,
+    },
+    -- Disable the Ctrl-click down event to stop programs from seeing it when a URL is clicked
+    {
+        event = { Down = { streak = 1, button = "Left" } },
+        mods = "CTRL",
+        action = wezterm.action.Nop,
+    },
+}
+
+search_mode = wezterm.gui.default_key_tables().search_mode
+table.insert(
+  search_mode,
+  { key = 'Enter', mods = 'SHIFT', action = act.CopyMode 'NextMatch' }
+ )
+
+config.key_tables = {
+  search_mode = search_mode
+}
+
 return config
+
